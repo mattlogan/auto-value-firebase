b161caadc209f9bd28a74fcd5c95af66e1022491
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2016-11-14 19:48:31 -0500

    Document @FirebaseAdapter

--- auto-value-firebase/src/test/java/me/mattlogan/auto/value/firebase/annotation/FirebaseAdapter.java
+++ auto-value-firebase/src/test/java/me/mattlogan/auto/value/firebase/annotation/FirebaseAdapter.java
@@ -9,6 +9,23 @@ import me.mattlogan.auto.value.firebase.TypeAdapter;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
+/**
+ * An annotation that indicates the auto-value-firebase {@link TypeAdapter} to use to
+ * serialize and deserialize the field.  The value must be set to a valid {@link TypeAdapter}
+ * class.
+ *
+ * <pre>
+ * <code>
+ * {@literal @}AutoValue @FirebaseValue public abstract class Foo {
+ *   {@literal @}FirebaseAdapter(DateTypeAdapter.class) public abstract Date date();
+ * }
+ * </code>
+ * </pre>
+ *
+ * The generated code will instantiate and use the {@code DateTypeAdapter} class to serialize and
+ * deserialize the {@code date()} property. In order for the generated code to instantiate the
+ * {@link TypeAdapter}, it needs a public, no-arg constructor.
+ */
 @Target(METHOD)
 @Retention(SOURCE)
 @Documented
